3485147edbbf2c56fde86f9bfdae96e9ef35e881
Sami Shukri <ss9td@mail.umkc.edu>
2016-11-07 16:37:55 -0600

    adding some classes

--- CS470/npc.h
+++ CS470/npc.h
@@ -1,1 +1,26 @@
 #pragma once
+#include <string>
+#include <list>
+
+using namespace std;
+
+class nPc {
+private: 
+	string fname, lname;
+	int id, bloodline;
+	int Age;
+	int loyaltyID;
+	attri npcAttri;
+	list<item> npcDrops;
+};
+
+
+typedef class npcAttribute {
+
+}* attri;
+
+
+class item {
+	int itemID;
+	string name, description;
+};
