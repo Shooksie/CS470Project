02194d626e4619a4b8a98ed3a59de34e237bb622
Sami Shukri <ss9td@mail.umkc.edu>
2016-11-13 18:05:27 -0600

    updates

--- CS470/npc.h
+++ CS470/npc.h
@@ -1,37 +1,64 @@
 #pragma once
 #include <string>
 #include <list>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 class nPc {
 private: 
 	string fname, lname;
-	int id, bloodline;
+	int iD, bloodline;
 	int Age;
-	int loyaltyID;
+	int loyalty;
 	int factionID;
 	int level;
 	int exp;
 	attri npcAttri;
-	list<item> npcDrops;
-
+	vector<item> npcDrops;
+public:
+	void setName(string first, string last) { fname = first;
+	lname = last;
+	};
+	void setID(int id) {
+		iD = id;
+	};
+	void setAge(int age) {
+		Age = age; };
+	void setLoyalty(int loyal) { 
+		loyalty = loyal; };
+	void setLevel(int levels) {
+		level = levels;
+	};
 };
 
 typedef class npcAttribute {
-	physical phsycials;
-	stats npcStats;
+	physical* physcials;
+	stats* npcStats;
+public:
+	void setPhyical(physical* physcial) { physcials = physcial; };
+	void setStats(stats* stat) {
+		npcStats = stat;};
+
 }* attri;
 
 
 class item {
 	int itemID;
 	string name, description;
+	map<string, int> effects;
+public: 
+	void setID(int id);
+	void setName(string itemName);
+	void setDesc(string desc);
 };
 
 
 class physical {
 	string hairID, eyesID, gender, bodyType, faceId;
+public:
+
 };
 
 class stats {
